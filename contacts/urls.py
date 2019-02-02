5b83045826d4548fdbf9ad350ababfd2fafd2896
akohrr <atadoga.akoh@gmail.com>
2019-02-02 20:17:14 +0100

    add forward slash '/' at the end of all contact urls

--- contacts/urls.py
+++ contacts/urls.py
@@ -6,9 +6,9 @@ app_name = 'contacts'
 
 
 urlpatterns = [
-    path('new', views.CreateContact.as_view(), name='new_contact'),
-    path('all', views.ListContact.as_view(), name='list_contact'),
-    path('detail/<int:pk>', views.ViewContact.as_view(), name='view_contact'),
-    path('update/<int:pk>', views.UpdateContact.as_view(), name='update_contact'),
-    path('delete/<int:pk>', views.DeleteContact.as_view(), name='delete_contact'),
+    path('new/', views.CreateContact.as_view(), name='new_contact'),
+    path('all/', views.ListContact.as_view(), name='list_contact'),
+    path('detail/<int:pk>/', views.ViewContact.as_view(), name='view_contact'),
+    path('update/<int:pk>/', views.UpdateContact.as_view(), name='update_contact'),
+    path('delete/<int:pk>/', views.DeleteContact.as_view(), name='delete_contact'),
 ]
