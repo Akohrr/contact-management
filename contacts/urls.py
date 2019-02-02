f7151ca10434b6f3fdd93e4d66b300fad028ff38
akohrr <atadoga.akoh@gmail.com>
2019-02-02 10:02:03 +0100

    update url name parameter for ViewContact url

--- contacts/urls.py
+++ contacts/urls.py
@@ -8,5 +8,5 @@ app_name = 'contacts'
 urlpatterns = [
     path('new', views.CreateContact.as_view(), name='new_contact'),
     path('all', views.ListContact.as_view(), name='list_contact'),
-    path('detail/<int:pk>', views.ViewContact.as_view(), name='list_contact'),
+    path('detail/<int:pk>', views.ViewContact.as_view(), name='view_contact'),
 ]
