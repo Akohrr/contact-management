cb0ba6a8a76c646b2717e3b775ef4fded5f76900
akohrr <atadoga.akoh@gmail.com>
2019-02-02 22:13:41 +0100

    add comments to contact app views on type of endpoint

--- contacts/views.py
+++ contacts/views.py
@@ -6,25 +6,29 @@ from .models import Contact
 from .serializers import ContactSerializer
 
 
+# POST Endpoint
 class CreateContact(generics.CreateAPIView):
     serializer_class = ContactSerializer
 
-
+# GET Endpoint
 class ListContact(generics.ListAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
+# GET Endpoint
 class ViewContact(generics.RetrieveAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
+# PUT, PATCH Endpoint
 class UpdateContact(generics.RetrieveUpdateAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
+# DELETE Endpoint
 class DeleteContact(generics.DestroyAPIView):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
