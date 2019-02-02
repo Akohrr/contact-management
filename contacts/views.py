b3fcd9af85c3f7aac9cef68b283628955800a00d
akohrr <atadoga.akoh@gmail.com>
2019-02-03 00:15:18 +0100

    update all contact views to use jwt for auth

--- contacts/views.py
+++ contacts/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Contact
 from .serializers import ContactSerializer
@@ -8,27 +9,32 @@ from .serializers import ContactSerializer
 
 # POST Endpoint
 class CreateContact(generics.CreateAPIView):
+    permission_classes = (IsAuthenticated, )
     serializer_class = ContactSerializer
 
 # GET Endpoint
 class ListContact(generics.ListAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
 # GET Endpoint
 class ViewContact(generics.RetrieveAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
 # PUT, PATCH Endpoint
 class UpdateContact(generics.RetrieveUpdateAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
 
 
 # DELETE Endpoint
 class DeleteContact(generics.DestroyAPIView):
+    permission_classes = (IsAuthenticated, )
     queryset = Contact.objects.all()
     serializer_class = ContactSerializer
