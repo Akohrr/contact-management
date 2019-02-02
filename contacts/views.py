86957fa6b9975cc3773bcd4293000a94ab58a349
akohrr <atadoga.akoh@gmail.com>
2019-02-02 08:48:27 +0100

    implement List api view for contacts

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import generics
+
+from .models import Contact
+from .serializers import ContactSerializer
+
+
+class ContactList(generics.ListCreateAPIView):
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
