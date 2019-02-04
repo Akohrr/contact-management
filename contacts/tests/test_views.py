f37297f611f2362e2c5235e68db0d469a21a6e9c
akohrr <atadoga.akoh@gmail.com>
2019-02-04 06:56:13 +0100

    Implement tests for PUT and PATCH endpoints of API

--- contacts/tests/test_views.py
+++ contacts/tests/test_views.py
@@ -61,6 +61,60 @@ class TestCreateContactView(BaseTestClass):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
 
+class TestUpdateContactView(BaseTestClass):
+    def setUp(self):
+        super().setUp()
+        test_data = {
+            'name': 'testname',
+            'address': 'testaddress',
+            'owner': self.user,
+            'work_phone': '2348123456789'
+        }
+        self.contact = Contact.objects.create(**test_data)
+        self.url = reverse('contacts:update_contact', kwargs={'pk': self.contact.id})
+
+    def test_unauthenticated_user(self):
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    # get requests allow you to view details of a contact
+    def test_get_request_with_authenticated_user(self):
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        response = self.client.get(self.url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    # PUT request allow you to modify contact instance
+    def test_put_request_with_authenticated_user(self):
+        new_test_data = {
+            'name': 'modifiedname',
+            'address': 'modifiedaddress',
+            'owner': self.user.id,
+            'work_phone': '23400000000'
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        response = self.client.put(self.url, new_test_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)# after database has been modified throught PUT  and PATCH request, aserrt that new data is in database
+        obj = Contact.objects.get(name='modifiedname')        
+        self.assertIsInstance(obj, Contact)
+        self.assertEqual(obj.name, 'modifiedname')
+
+    # PATCH request allow you to modify contact instance
+    def test_patch_request_with_authenticated_user(self):
+        new_test_data = {
+            'name': 'modifiedname',
+            'address': 'modifiedaddress',
+            'owner': self.user.id,
+            'work_phone': '23400000000'
+        }
+        self.client.credentials(HTTP_AUTHORIZATION='Bearer ' + self.access_token)
+        response = self.client.patch(self.url, new_test_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # after database has been modified throught PUT  and PATCH request, aserrt that new data is in database
+        obj = Contact.objects.get(name='modifiedname')        
+        self.assertIsInstance(obj, Contact)
+        self.assertEqual(obj.name, 'modifiedname')
+
+
 # test for DELETE request
 class TestDeleteContactView(BaseTestClass):
     def setUp(self):
